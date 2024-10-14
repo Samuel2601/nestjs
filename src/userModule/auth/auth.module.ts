7da433dd1a53066d941885606849925443a003cf
Samuel2601 <saamare99@gmail.com>
2024-10-14 13:06:23 -0500

    login 3

--- src/userModule/auth/auth.module.ts
+++ src/userModule/auth/auth.module.ts
@@ -4,6 +4,7 @@ import {AuthService} from './auth.service';
 import {MongooseModule} from '@nestjs/mongoose';
 import {User, UserSchema} from '../models/user.schema';
 import { JwtModule } from '@nestjs/jwt';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
 	imports: [
@@ -17,6 +18,7 @@ import { JwtModule } from '@nestjs/jwt';
 			secret: process.env.JWT_SECRET, // La clave secreta para firmar los tokens
 			signOptions: {expiresIn: '60s'}, // Opciones de firma
 		}),
+		HttpModule
 	],
 	providers: [AuthService],
 	controllers: [AuthController],
