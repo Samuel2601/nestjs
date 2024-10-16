285abd28ccc4f0ac633bb70a4517b4f31aa85b0d
Samuel2601 <saamare99@gmail.com>
2024-10-16 11:33:56 -0500

    iniciadores de permisos y roles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
-
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {DatabaseModule} from './database/database.module';
 import {userAppModule} from './userModule/userApp.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './userModule/models/user.schema';
+import { RoleUser, RoleUserSchema } from './userModule/models/roleuser.schema';
+import { Permission, PermissionSchema } from './userModule/models/permiso.schema';
 
 @Module({
-	imports: [DatabaseModule, userAppModule ],
+	imports: [
+		DatabaseModule,
+		userAppModule,
+		MongooseModule.forFeature([
+			{
+				name: User.name,
+				schema: UserSchema,
+			},
+			{name: RoleUser.name, schema: RoleUserSchema},
+			{name: Permission.name, schema: PermissionSchema},
+		]),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
