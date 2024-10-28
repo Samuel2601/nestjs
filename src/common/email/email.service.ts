8992cf355ab03068f94aef22b553e6b885878f8c
Samuel2601 <saamare99@gmail.com>
2024-10-28 12:45:08 -0500

    ajsjasjdqweqwe

--- src/common/email/email.service.ts
+++ src/common/email/email.service.ts
@@ -1,55 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import * as nodemailer from 'nodemailer'; // Usa * para importar todo el módulo de nodemailer
 import * as fs from 'fs'; // Asegúrate de usar la forma correcta para importar módulos nativos de Node.js
 import * as handlebars from 'handlebars';
+import {ConfigService} from '@nestjs/config';
 
 @Injectable()
 export class EmailService {
-  private transporter: nodemailer.Transporter; // Usa el tipo correcto para el transporter
+	private transporter: nodemailer.Transporter; // Usa el tipo correcto para el transporter
 
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      host: "mail.esmeraldas.gob.ec",
-      port: 465,
-      secure: true,
-      auth: {
-        user: "aplicaciones@esmeraldas.gob.ec",
-        pass: "Alcaldia2024/*",
-      },
-    });
-  }
+	constructor(private configService: ConfigService) {
+		this.transporter = nodemailer.createTransport({
+			host: this.configService.get<string>('EMAIL_HOST'),
+			port: this.configService.get<number>('EMAIL_PORT'),
+			secure: true,
+			auth: {
+				user: this.configService.get<string>('EMAIL_USER'),
+				pass: this.configService.get<string>('EMAIL_PASS'),
+			},
+		});
+	}
 
-  async sendNotification(to: string, subject: string, templatePath: string, templateData: any) {
-    try {
-      // Lee el archivo HTML
-      const html = await this.readHTMLFile(templatePath);
-      // Compón el contenido del correo
-      const template = handlebars.compile(html);
-      const htmlToSend = template(templateData);
+	async sendNotification(to: string, subject: string, templatePath: string, templateData: any) {
+		try {
+			// Lee el archivo HTML
+			const html = await this.readHTMLFile(templatePath);
+			// Compón el contenido del correo
+			const template = handlebars.compile(html);
+			const htmlToSend = template(templateData);
 
-      const mailOptions = {
-        from: "aplicaciones@esmeraldas.gob.ec",
-        to,
-        subject,
-        html: htmlToSend,
-      };
+			const mailOptions = {
+				from: this.configService.get<string>('EMAIL_USER'),
+				to,
+				subject,
+				html: htmlToSend,
+			};
 
-      // Envía el correo
-      await this.transporter.sendMail(mailOptions);
-      console.log('Email sent successfully');
-    } catch (error) {
-      console.error('Error sending email:', error);
-    }
-  }
+			// Envía el correo
+			await this.transporter.sendMail(mailOptions);
+			console.log('Email sent successfully');
+		} catch (error) {
+			console.error('Error sending email:', error);
+		}
+	}
 
-  private readHTMLFile(filePath: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, { encoding: 'utf-8' }, (err, html) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(html);
-      });
-    });
-  }
+	private readHTMLFile(filePath: string): Promise<string> {
+		return new Promise((resolve, reject) => {
+			fs.readFile(filePath, {encoding: 'utf-8'}, (err, html) => {
+				if (err) {
+					return reject(err);
+				}
+				resolve(html);
+			});
+		});
+	}
 }
